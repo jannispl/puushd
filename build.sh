509c1fe8e59c7deb2b7c7ad0afa3966760995ef5
mave <me@mavedev.com>
2012-11-04 23:08:06 +0100

    Woops. windows did not like that

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 gcc *.c -c
 
-g++ -std=c++0x *.cpp ./MPFDParser/*.cpp ./pages/*.cpp sqlite3.o mongoose.o md5.o -o puushd -ldl -lpthread
+g++ -std=c++0x -I./ *.cpp ./MPFDParser/*.cpp ./pages/*.cpp sqlite3.o mongoose.o md5.o -o puushd -ldl -lpthread
 
 rm *.o
